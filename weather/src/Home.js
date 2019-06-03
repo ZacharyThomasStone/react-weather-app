a0d7369791be8fbf1a563742da0a992cbcd8c519
Zachary Stone <zacharythomasstone89@gmail.com>
2019-06-03 13:18:48 -0400

    Weather Card UI
    Added basic UI for Weather card

--- weather/src/Home.js
+++ weather/src/Home.js
@@ -1,5 +1,25 @@
 import React from "react";
+import {Col, Row} from "antd";
+import {WeatherCard} from "./WeatherCard";
 
 export function Home() {
-    return <h2>Home</h2>;
+    return <div style={{background: '#ECECEC', padding: '30px'}}>
+        <Row gutter={10}>
+            <Col span={2}>
+                <WeatherCard
+                    title={"Current Temp"}
+                    value={"75"}
+                    suffix={"°"}
+                />
+
+            </Col>
+            <Col span={2}>
+            <WeatherCard
+                title={"Tuesday"}
+                value={"5/21/2019"}
+
+            />
+            </Col>
+        </Row>
+    </div>
 }
